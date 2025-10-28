3979f44c36379010bc75648aa0deba925ee9ec1e
MaartentePoele <maarten.te.poele@hva.nl>
2025-10-28 11:44:46 +0100

    #9 Comments toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
+// Selecteerd de button en de header
 const btn = document.querySelector("button");
 const header = document.querySelector("header");
 
+// Als je op de button klikt wordt de toggleColour uitgevoerd
 btn.addEventListener("click", toggleColour);
 
+// Functie dat de class van de header veranderd
 function toggleColour() {
   header.classList.toggle("header-nieuw-west-betere-kleur");
 }
